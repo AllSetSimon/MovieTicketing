f301865186def84ca56b0134fa52c439bdb8d886
svra0 <svra0@LAPTOP-EVN99THA>
2021-08-24 18:10:12 +0900

    장동주 8월24일 자 Login.java , loginData.txt , MyCalendar.java

--- src/Login.java
+++ src/Login.java
@@ -3,38 +3,54 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Login {
 	
-	static HashMap<String, String> members ;
-	static int tryCount = 2;
-	public static void main(String[] args) throws IOException {
-		boolean bLogin= false;
-		bLogin=Login.loginProcess();
+	class LoginData{ // 로그인 데이터 저장용 클래스
+		private String id;
+		private String pwd;
+		private String nickname;
+		
+		LoginData(String id , String pwd, String nickname){
+			this.id = id;
+			this.pwd = pwd;
+			this.nickname = nickname;
+		}
+	}
 	
-		if(bLogin) {
+	
+	static ArrayList<LoginData> loginDatas;
+	int tryCount = 2;
+	
+	
+	
+	public static void main(String[] args)  {
+		Login login = new Login(); // 객체 생성
+		boolean bLogin= false;	
+		bLogin=login.loginProcess(); // 로그인 프로세스 실행
+	
+		if(bLogin) { // 로그인 성공
 			System.out.println("메인메뉴 출력");
 		}
-		else {
+		else { // 로그인 실패
 			System.out.println("프로그램 종료");
 		}
-		
 	}
 	
 	
 	
-	static boolean loginProcess() throws IOException {
+	boolean loginProcess() {
 
 		boolean bLogin = false;
 		ReadFile();
-		
 		System.out.println("환영합니다! 아이디와 비밀번호를 입력해주세요");
-		while(tryCount > 0) {
-			bLogin =Login.tryLogin();
-			tryCount--;
+		while(tryCount > 0) { //시도 횟수 만큼 로그인 도전
+			bLogin =tryLogin(); // 로그인 시도
+			tryCount--; // 기회 감소
 			if(bLogin)
 				break;
 		}
@@ -46,28 +62,29 @@ public class Login {
 	}
 	
 	
-	static void ReadFile() throws IOException {
-		members	= new HashMap<String,String>();
+	 void ReadFile() {
+		loginDatas	= new ArrayList<LoginData>();
 		File file = new File("./src/loginData.txt");
+		try {
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		
 		String line = null;
 		
-		while( (line = br.readLine() ) != null ) {
-			String id = null;
-			String pwd = null;
+		while( (line = br.readLine() ) != null ) { // 파일 읽으면서 데이터 저장
 			String[] pieces= line.split("/");
-		
-			members.put(pieces[0], pieces[1]);
+			loginDatas.add(new Login.LoginData(pieces[0] , pieces[1] , pieces[2] )  ); 
+		}
+		}catch  (Exception e){
+			System.out.println("파일이 이상합니다.");
 		}
 	}
 	
 	
 	
-	static boolean tryLogin() {
+	 boolean tryLogin() {
 		String id = null;
 		String pwd = null;
-		boolean bFindId =false;
+		int findIndex = -1;
 		Scanner scan  = new Scanner(System.in);
 
 		System.out.print("아이디:");
@@ -75,31 +92,30 @@ public class Login {
 		System.out.print("비밀번호:");
 		pwd =scan.nextLine();
 		
-		for(String key: members.keySet()) {
-			if(id.equals(key)) {
-				bFindId = true;
+		for(LoginData logindata: loginDatas) {  //입력된 아이디와 일치하는 데이터 찾기
+			if(id.equals(logindata.id)) {
+				findIndex = loginDatas.indexOf(logindata); //찾은 인덱스 저장
 			}
-			if(bFindId)
+			if(findIndex != -1)  // 인덱스 찾았으면 반복문 탈출
 				break;
 		}
-		if(bFindId == false){
-			if(tryCount == 1)
+		if(findIndex == -1){    //아이디를 못찾은 경우
+			if(tryCount == 1) //마지막 기회였을 경우
 				System.out.println("회원등록 여부 확인 후 재접속 부탁드립니다");
-			else
+			else  // 기회 남았을때
 			System.out.println("아이디가 없습니다. 다시 입력해주세요");
 			return false;
 		}
-		else {
-			if(members.get(id).equals(pwd)) {
-				System.out.println(id +"님 환영 합니다.");
+		else { //아이디 찾은 경우
+			if(loginDatas.get(findIndex).pwd.equals(pwd)) { // 비밀번호도 맞는 경우
+				System.out.println(loginDatas.get(findIndex).nickname +"님 환영 합니다."); //닉네임 출력 
 				return true;
 			}
-			else {
-				if(tryCount == 1)
+			else { // 비밀번호 다름
+				if(tryCount == 1) //마지막 기회였을 경우
 				System.out.println("회원등록 여부 확인 후 재접속 부탁드립니다");
-				else
+				else // 기회 남았을때
 				System.out.println("일치하는 회원정보가 없습니다. 다시 입력해주세요");
-				
 					
 				return false;
 			}
--- src/MyCalendar.java
+++ src/MyCalendar.java
@@ -4,70 +4,93 @@ import java.util.Scanner;
 public class MyCalendar {
 
 	private String date;
-	static int mYear = 2021;
-	static int mMonth = 8;
+	int mYear = 2021;
+	int mMonth = 8;
+	Calendar start = Calendar.getInstance();
+	Calendar end = Calendar.getInstance();
+	
+	
+//	public static void main(String[] args) {
+//			MyCalendar mycalendar= new MyCalendar();
+//			String result = null;
+//			result = mycalendar.selecteDate();
+//			System.out.println("선택된 날짜:"+result);
+//
+//		
+//		}
 
-	public static void main(String[] args) {
-			String result = null;
-			result = MyCalendar.selecteDate();
-			System.out.println("선택된 날짜:"+result);
-
-		
-		}
-
-	public static  String selecteDate() { // 선택가능한 날짜 달력 출력, 날짜선택, 데이터 리턴
+	public String selecteDate() { // 선택가능한 날짜 달력 출력, 날짜선택, 데이터 리턴
 		Scanner sc = new Scanner(System.in);
 		String input = null;
 		while (true) {
-			MyCalendar.displayCalendar();
+			displayCalendar();
 			System.out.println();
 			System.out.println("날짜를 입력해주세요: (선택:숫자입력 , > : 다음달  <: 이전달)");
 
 			input = sc.nextLine();
 			if (input.equals(">")) {
-				MyCalendar.mMonth++;
+				mMonth++;
 			}else if (input.equals("<")) {
-				MyCalendar.mMonth--;
+				mMonth--;
+			}else if (isNumber( input )) { // 숫자 인지 체크
+				int nInput = Integer.parseInt(input);
+				
+				if(nInput <=end.get(Calendar.DATE) && nInput > 0 ) // 선택 가능한 날짜인지 체크
+					return mYear+"-"+mMonth+"-"+input;
+				else
+					System.out.println("유효 숫자 범위가 아닙니다.");
 			}
 			else
-				break;
+				System.out.println("올바르게 입력해주세요");
 			
 		}
-		return MyCalendar.mYear+"-"+MyCalendar.mMonth+"-"+input;
+		
 	}
 
-	static void displayCalendar() {
+	void displayCalendar() {
 
 		int year = mYear;
 		int month = mMonth;
 		int START_DAY_OF_WEEK = 0;
 		int END_DAY = 0;
+		
+		
+		start.set(year, month - 1, 1);  // 디스플레이 할 달에 첫날로 세팅 (Calendar 는 달을 0 ~ 11로 저장해서 month-1 이 해당 달)
+		end.set(year, month, 1); 		// 디스플레이 다음달에 첫날로 세팅
+		end.add(Calendar.DATE, -1);		// 다음달 첫날에서 하루 뺴서 디스플레이 할 달에 마지막 날짜로 세팅
 
-		Calendar start = Calendar.getInstance();
-		Calendar end = Calendar.getInstance();
-
-		start.set(year, month - 1, 1);
-		end.set(year, month, 1);
-		end.add(Calendar.DATE, -1);
-
-		START_DAY_OF_WEEK = start.get(Calendar.DAY_OF_WEEK);
-		END_DAY = end.get(Calendar.DATE);
-
+		START_DAY_OF_WEEK = start.get(Calendar.DAY_OF_WEEK); // 첫날에 요일 구하기 1:일요일 7:토요일
+		END_DAY = end.get(Calendar.DATE); // 마지막 일자 구하기
 		System.out.println(year + "년 " + month + "월 달력\n" + "일\t월\t화\t수\t목\t금\t토");
 
-		for (int q = 1; q < START_DAY_OF_WEEK; q++) {
+		for (int q = 1; q < START_DAY_OF_WEEK; q++) {   // 첫날 요일 까진 공백
 			System.out.print("\t");
 		}
 
 		int cnt = START_DAY_OF_WEEK - 1;
-		for (int q = 1; q <= END_DAY; q++) {
+		for (int q = 1; q <= END_DAY; q++) { //1일부터 마지막 날까지 출력
 			System.out.print(q + "\t");
 			cnt++;
-			if (cnt == 7) {
+			if (cnt == 7) { //토요일 출력 후 마다 개행
 				cnt = 0;
 				System.out.println("\n");
 			}
 		}
 
+	}
+	
+	boolean isNumber(String str) { // 넘겨받은 문자열이 숫자 인지 체크
+		char letter;
+		
+		if(str.equals("")) {
+			return false;
+		}
+		
+		for(int i = 0 ; i<str.length();i++) {
+			letter = str.charAt(i);
+			if (!Character.isDigit(letter))
+				return false;
+		}
+		return true;
 	}
 }
