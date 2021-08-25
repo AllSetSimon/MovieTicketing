45122af01673833a73e2c48c54858930c0e5779c
AllSetSimon <allsetsimon@gmail.com>
2021-08-25 14:00:56 +0900

    구현님 Theater 추가 구현 과제

--- src/SystemManager.java
+++ src/SystemManager.java
@@ -6,13 +6,16 @@ public class SystemManager {
 
 	ArrayList<Theater> theaterList = new ArrayList<Theater>();
 	ArrayList<Movie> showingList = new ArrayList<Movie>();
+	ArrayList<String> resultList = new ArrayList<String>();;
+
+	String mvName;
 
 	public SystemManager() {
 		// Movie m1 = new Movie(title, genre, director, actor, plot, release, rating)
 		Movie sinkHole = new Movie("싱크홀", "액션", "봉찬욱", "이광수", "싱크홀줄거리", "2021-08-15", 0.0);
 		Movie bossBB = new Movie("보스베이비2", "애니메이션", "세스로건", "보스", "보스베이비줄거리", "2021-08-07", 0.0);
 		Movie mogaDS = new Movie("모가디슈", "액션", "박찬봉", "조인성", "모가디슈줄거리", "2021-08-12", 0.0);
-		
+
 		showingList.add(sinkHole);
 		showingList.add(mogaDS);
 		showingList.add(bossBB);
@@ -37,20 +40,20 @@ public class SystemManager {
 	}
 
 	public void nowShowing() {
-		//상영중인 영화 리스트 출력
+		// 상영중인 영화 리스트 출력
 		System.out.println("======================");
 		System.out.println("현재 상영중인 영화입니다");
 		System.out.println("======================");
-		
+
 		for (int i = 0; i < showingList.size(); i++) {
 			System.out.println((i + 1) + ". " + showingList.get(i).getTitle());
 		}
-		
-		System.out.println("======================");		
+
+		System.out.println("======================");
 	}
-	
+
 	public void showDetail(int selectNum) {
-		//영화 상세정보 출력
+		// 영화 상세정보 출력
 		System.out.println("======================");
 		System.out.println("제목 :" + showingList.get(selectNum - 1).getTitle());
 		System.out.println("장르 :" + showingList.get(selectNum - 1).getGenre());
@@ -59,37 +62,26 @@ public class SystemManager {
 		System.out.println("개봉일 :" + showingList.get(selectNum - 1).getRelease());
 		System.out.println("관객평점 :" + showingList.get(selectNum).getRating());
 		System.out.println("줄거리 :" + showingList.get(selectNum - 1).getPlot());
-		
-	}
-	
-	public void showTheater() {
-		//Scanner sc = new Scanner(System.in);
-		
-		ArrayList<String> resultList = new ArrayList<>();
-		HashMap<String, String> map = new HashMap<>();
-		for (int i = 0; i < theaterList.size(); i++) {
-			for (int j = 0; j < showingList.size(); j++) {
-				resultList.add(showingList.get(j).getTitle()); 
-				resultList.add(theaterList.get(i).getTheaterName()); 
-				
-				//map.put(showingList.get(j).getTitle(), theaterList.get(i).getTheaterName());
-			}
-		}
-	
-		/*for (String key : map.keySet()) {
-			System.out.println(key + map.get(key));
-		}*/
-		
-		for (String result : resultList) {
-			System.out.println(result);
-		}
-		
-			
-	}
-	public static void main(String[] args) {
-		SystemManager sm = new SystemManager();
-		sm.showTheater();
-		
+
 	}
 
+	public void showTheater(int selectNum) {
+		// mvName = showingList에서 내가 선택한 인덱스 값을 가져오도록 설정
+
+		resultList.clear();
+
+		// map에서 get(key값)
+		// p key : 3, value : "C"
+		// p.get(3) ==> String ["C"]
+
+		/*
+		 * for (int i = 0; i < 어떤게 들어와야 할지; i++) { //getMovieMap().get(key) ==> 결과값을
+		 * resultList에 add하겠다. }
+		 */
+
+		// resultList를 for문을 통해서 그 안에 있는 내용을 다 꺼내는 거에요
+		// 1. 2. (index+1)값이 붙도록 --> 향상for문X
+
+		//3단계 해주세요
+	}
 }
