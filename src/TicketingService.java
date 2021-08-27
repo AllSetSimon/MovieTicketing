1f339e4465d23f9ab45131763ba4f45694fcaa11
AllSetSimon <allsetsimon@gmail.com>
2021-08-27 16:09:52 +0900

    메뉴 선택 관련 예외처리

--- src/TicketingService.java
+++ src/TicketingService.java
@@ -23,34 +23,39 @@ public class TicketingService {
 			System.out.println("        6. 종료");
 			System.out.println("==============================");
 			System.out.print("원하시는 메뉴를 선택해주세요 : ");
-			int select = Integer.parseInt(sc.nextLine()); 
-
-			switch (select) {
-			case 1:
-				sm.nowShowing(1);
-				break;
-			case 2:
-				sm.nowShowing(2);
-				break;
-			case 3:
-				sm.nowShowing(3);
-				break;
-			case 4:
-				sm.checkRsv();
-				break;
-			case 5:
-				bFlagWhile = sm.reLogin();
-				break;
-			case 6:
-				System.out.println("프로그램을 종료합니다");
-				System.exit(0);
-				break;
-			default:
-				System.out.println("잘못 누르셨습니다");
-				break;
+			int select = 0;
+			try {
+				select = Integer.parseInt(sc.nextLine()); 
+				if(select < 7) {
+					switch (select) {
+					case 1:
+						sm.nowShowing(1);
+						break;
+					case 2:
+						sm.nowShowing(2);
+						break;
+					case 3:
+						sm.nowShowing(3);
+						break;
+					case 4:
+						sm.checkRsv();
+						break;
+					case 5:
+						bFlagWhile = sm.reLogin();
+						break;
+					case 6:
+						System.out.println("프로그램을 종료합니다");
+						System.exit(0);
+						break;
+					default:
+						break;
+					}
+				} else {
+					System.out.println("번호를 잘못 입력하셨습니다.");
+				}
+			}catch (Exception e) {
+				System.out.println("잘못된 형식의 값입니다.");
 			}
-			
 		}
 	}
-
 }
